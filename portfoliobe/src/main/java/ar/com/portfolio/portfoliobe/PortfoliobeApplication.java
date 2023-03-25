cbbd8db3663bfc0ed288afa0e5c5dd8f4a0a2b7b
ferbonelli <bonelli.fernando@gmail.com>
2023-03-25 13:01:03 -0300

    Primera subida Backend

---
+++ portfoliobe/src/main/java/ar/com/portfolio/portfoliobe/PortfoliobeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.portfolio.portfoliobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliobeApplication.class, args);
+	}
+
+}
---
+++ portfoliobe/src/test/java/ar/com/portfolio/portfoliobe/PortfoliobeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.portfolio.portfoliobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

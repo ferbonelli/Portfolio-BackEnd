2b477b28adda6995590ee8f1395598f3144028bd
ferbonelli <bonelli.fernando@gmail.com>
2023-04-08 10:40:36 -0300

    Backend con CORS_OK

--- portfoliodb/src/main/java/com/portfolio/portfoliodb/PortfoliodbApplication.java
+++ portfoliodb/src/main/java/com/portfolio/portfoliodb/PortfoliodbApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.portfoliodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfoliodbApplication {
@@ -9,5 +12,15 @@ public class PortfoliodbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfoliodbApplication.class, args);
 	}
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+                }
+            };
+        }
 
 }
--- portfoliodb/src/main/java/com/portfolio/portfoliodb/controller/Controller.java
+++ portfoliodb/src/main/java/com/portfolio/portfoliodb/controller/Controller.java
@@ -24,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class Controller {
     
